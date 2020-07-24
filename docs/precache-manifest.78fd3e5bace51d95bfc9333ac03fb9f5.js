5b317994ac4894ca898f16c4a6e640007b4747fa
RaulMG <raul.mtzglez@gmail.com>
2020-07-24 13:08:22 -0500

    Docs agregado

---
+++ docs/precache-manifest.78fd3e5bace51d95bfc9333ac03fb9f5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "143371ef34b80968085a5d310be05068",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9378d362519db493e453",
+    "url": "/static/css/main.6e47dca8.chunk.css"
+  },
+  {
+    "revision": "3ec50c8fead662706b17",
+    "url": "/static/js/2.637c77a8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.637c77a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9378d362519db493e453",
+    "url": "/static/js/main.1c61a24c.chunk.js"
+  },
+  {
+    "revision": "2beaf3c9299542d4240e",
+    "url": "/static/js/runtime-main.c8ba4116.js"
+  }
+]);
